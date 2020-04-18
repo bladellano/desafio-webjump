628fdb421f15df8c6f37903470a4b3c5b9d071cd
Caio Silva <bladellano@gmail.com>
2020-04-18 20:35:16 -0300

    Configurando

--- core/Config.php
+++ core/Config.php
@@ -3,13 +3,14 @@
 session_start();
 ob_start();
 
-define('URL','http://localhost/desafio/');
+// define('URL','http://localhost/desafio/');
+define('URL','https://desafio-webjump.herokuapp.com/');
 
 define('CONTROLLER','Home');
 define('METODO','index');
 
 //Crendenciais de acesso ao BD
-define('HOST','localhost');
-define('USER','root');
-define('PASS','');
-define('DBNAME','webjump');
+define('HOST','dellanosites.com.br');
+define('USER','root_webjump');
+define('PASS','Tylo_796');
+define('DBNAME','dellanos_webjump');
